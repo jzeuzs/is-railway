14752bff281f21942abaa1196f59368a67e6c2b9
Yoshida Tomio <mail@tomio.codes>
2021-08-29 15:48:03 +0800

    fix: module not found

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
 import isRailway from '.';
+import updateNotifier from 'update-notifier';
 
+// Disabled this rule because we cant import the package.json file because typescript complains
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require('../package.json');
+
+updateNotifier({ pkg }).notify();
 process.exitCode = isRailway() ? 0 : 2;
